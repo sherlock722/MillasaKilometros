d66f0f1f5f546777eff0ede1866742ca14de333e
Javier Cazorla <sherlock722@gmail.com>
2016-09-01 17:25:09 +0200

    App finalizada aunque mejorable

--- Millas a Kilometros/Millas a Kilometros/ViewController.swift
+++ Millas a Kilometros/Millas a Kilometros/ViewController.swift
@@ -9,10 +9,88 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    
+    @IBOutlet var distanceTextfield: UITextField!
+    
+    @IBOutlet var segmentController: UISegmentedControl!
+    
+    @IBOutlet var resultLabel: UILabel!
+    
+    
+    let milesUnit = 1.609
+    
+    
+    @IBAction func ConvertButton(_ sender: UIButton) {
+        
+        let selectedindex = segmentController.selectedSegmentIndex
+        
+        let textFieldValue = Double (distanceTextfield.text!)!
+        
+        
+        if selectedindex == 0 {
+            
+            //Convertir a Miles
+            let convertedValue = textFieldValue / milesUnit
+            reloadView(textFieldValue: textFieldValue, convertedValue: convertedValue)
+            
+            //let initValue = String(format:"%.2f", textFieldValue)
+            //let endValue = String(format: "%.2f", convertedValue)
+            //resultLabel.text = "\(initValue) km = \(endValue) miles"
+            //print ("Pasar a Millas \(convertedValue)")
+       
+        }else {
+            
+            //Convertir a KM
+            let convertedValue = textFieldValue * milesUnit
+            reloadView(textFieldValue: textFieldValue, convertedValue: convertedValue)
+            
+            
+            //let initValue = String(format:"%.2f", textFieldValue)
+            //let endValue = String(format: "%.2f", convertedValue)
+            //resultLabel.text = "\(initValue) miles = \(endValue) km"
+            //print ("Pasar a Kilómetros \(convertedValue)")
+        }
+        
+    }
+    
+    func reloadView(textFieldValue : Double, convertedValue : Double){
+        
+        
+        let initValue = String(format:"%.3f", textFieldValue)
+        let endValue = String(format: "%.2f", convertedValue)
+        
+        if segmentController.selectedSegmentIndex == 0 {
+            
+            resultLabel.text = "\(initValue) km = \(endValue) miles"
+            
+        } else {
+            
+            resultLabel.text = "\(initValue) miles = \(endValue) km"
+            
+        }
+        
+        
+        
+    }
+    
+    //Quitar la barra de estado (bateria y hora) Proiedad prefersStatusBarHidden
+    override var prefersStatusBarHidden: Bool{
+        get{
+            return false
+        }
+    }
+    
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        //Cambiar el texto de la etiqueta de resultado
+        resultLabel.text="Resultado de la Operación"
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
